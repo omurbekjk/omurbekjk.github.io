6e01e3ce98c7d4fe549191b75b0ad93da00dbb57
omurbekjk <omurbek.jk@gmail.com>
2018-02-19 17:08:39 +0600

    dev: bootstrap4 changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config:  {
+    apiKey: 'AIzaSyBaoc6wR_e1ul0LnUrE-99wK0jJprJ-cgU',
+    authDomain: 'omurbek-jk.firebaseapp.com',
+    databaseURL: 'https://omurbek-jk.firebaseio.com',
+    projectId: 'omurbek-jk',
+    storageBucket: '',
+    messagingSenderId: '788833455886'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config:  {
+    apiKey: 'AIzaSyBaoc6wR_e1ul0LnUrE-99wK0jJprJ-cgU',
+    authDomain: 'omurbek-jk.firebaseapp.com',
+    databaseURL: 'https://omurbek-jk.firebaseio.com',
+    projectId: 'omurbek-jk',
+    storageBucket: '',
+    messagingSenderId: '788833455886'
+  }
 };
+
