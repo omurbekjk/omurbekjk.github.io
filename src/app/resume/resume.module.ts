266219371f54141ba7b2b8c55bbb59c557581d89
omurbek <omurbek.jk@gmail.com>
2018-06-24 22:28:39 +0600

    added components

--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -4,6 +4,10 @@ import {CommonModule} from '@angular/common';
 import {ResumeRoutingModule} from './resume-routing.module';
 import {ResumeComponent} from './resume.component';
 import {WhoamiComponent} from './whoami/whoami.component';
+import {AboutComponent} from './about/about.component';
+import {AwardsComponent} from './awards/awards.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {SkillsComponent} from './skills/skills.component';
 
 @NgModule({
   imports: [
@@ -13,6 +17,10 @@ import {WhoamiComponent} from './whoami/whoami.component';
   declarations: [
     ResumeComponent,
     WhoamiComponent,
+    AboutComponent,
+    AwardsComponent,
+    PortfolioComponent,
+    SkillsComponent,
   ]
 })
 export class ResumeModule {
---
+++ src/app/resume/skills/skills.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {skills} from '../skills';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
+})
+export class SkillsComponent implements OnInit {
+  skills;
+
+  constructor() {
+    this.skills = skills;
+  }
+
+  ngOnInit() {
+  }
+
+}
